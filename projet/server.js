e3f68936e85e344760fc526eca8a1f24121d511d
root <root@vps-5afe3273.vps.ovh.net>
2021-08-26 14:53:00 +0000

    update fichiers

--- projet/server.js
+++ projet/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const fetch = require('node-fetch');
 const path = require('path');
 
